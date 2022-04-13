20e7e36c395f9567e4be4d8376207b52638d4ef2
INU YSJ <xagrant90@ac.kr>
2022-04-13 14:17:10 +0900

    연속행렬곱셈 자바 최종본

---
+++ MatrixChain_Final.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class MatrixChain_Final {
+    static int[][] matrix;
+    static int[][] c;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("곱할 행렬의 개수를 입력 : ");
+        n = scanner.nextInt();
+        matrix = new int[n][2];
+        c = new int[n][n];
+
+        System.out.println("각 행렬의 크기를 입력");
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = scanner.nextInt();
+            matrix[i][1] = scanner.nextInt();
+        }
+
+        minimum(0, n - 1);
+        System.out.println("곱셈횟수의 최솟값은 : " + c[0][n - 1]);
+
+        scanner.close();
+    }
+
+    public static int minimum(int x, int y)
+    {
+        int result = 1234567890;
+
+        if (x == y)
+        {
+            return 0;
+        }
+
+        if (c[x][y] != 0)
+        {
+            return c[x][y];
+        }
+
+        for (int i = x; i < y; i++)
+        {
+            result = Math.min(result, minimum(x, i) + minimum(i + 1, y) + matrix[x][0] * matrix[i][1] * matrix[y][1]);
+            c[x][y] = result;
+        }
+
+        return result;
+    }
+}
